935eb740d9a1d35f263f2b71dcd0b6c6a51ed7bf
merlititillart <merliti.tillart@khk.ee>
2018-12-06 19:23:41 +0200

    deleted plugin `Social Media Links` version 1.0.3

--- wp-content/plugins/social-media-links/admin/js/social-media-admin.js
+++
@@ -1,8 +0,0 @@
-jQuery(function ($) {
-	jQuery(document).ready(function(){	
-	document.getElementById('sm_social_shortcode').readOnly = true;
-	});
-        
-  $('.viewsociallink tbody').sortable();
-
-});
